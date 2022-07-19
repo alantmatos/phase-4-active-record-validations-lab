3ae69a363c5ec277407a42c3544eaac0abf729fd
Alan Torquatro de Matos <mr.alanmatos@hotmail.com>
2022-07-19 15:56:09 -0400

    commit tree f0ae4839ac37a5ed6d1e5c074951b1b166dfb637

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,2 +1,5 @@
 class Author < ApplicationRecord
+    validates :name, presence: true, uniqueness: true
+    validates :name, confirmation: { case_sensitive: false }
+    validates :phone_number, length: { is: 10 }
 end
